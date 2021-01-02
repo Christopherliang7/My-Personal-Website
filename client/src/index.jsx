82585cdd5a6bdda21804a2c4adbb716c1e91a2f0
Christopher <Christopherliang7@gmail.com>
2021-01-01 19:35:49 -0500

    Complete Initial Set Up

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      someState: "value of state"
-    };
-  }
-
-  render() {
-    return <div>Hello! Does this work?</div>;
-  }
+const App = () => {
+  return (
+    <>
+      <div>Hello! Does this work?</div>
+    </>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById("app")); 
